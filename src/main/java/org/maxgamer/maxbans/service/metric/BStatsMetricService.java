380eb5c2ce33e58dec1d119b842ae66667f0f195
Jacob Crofts <rmrfinbox@gmail.com>
2022-08-24 15:18:40 -0700

    Publish this project to ManageMC

--- src/main/java/org/maxgamer/maxbans/service/metric/BStatsMetricService.java
+++ src/main/java/org/maxgamer/maxbans/service/metric/BStatsMetricService.java
@@ -1,6 +1,5 @@
 package org.maxgamer.maxbans.service.metric;
 
-import org.bstats.bukkit.Metrics;
 import org.maxgamer.maxbans.MaxBansPlus;
 
 import javax.inject.Inject;
@@ -12,45 +11,37 @@ import java.util.concurrent.Callable;
  * @author netherfoam
  */
 public class BStatsMetricService implements MetricService {
-    /**
-     * bStats Link: https://bstats.org/plugin/bukkit/MaxBansPlus/829
-     */
-    public static final int MAXBANS_PLUS_PLUGIN_ID = 829;
+  /**
+   * bStats Link: https://bstats.org/plugin/bukkit/MaxBansPlus/829
+   */
+  public static final int MAXBANS_PLUS_PLUGIN_ID = 829;
 
-    private Map<String, Integer> increments = new HashMap<>();
+  private Map<String, Integer> increments = new HashMap<>();
 
-    @Inject
-    public BStatsMetricService(MaxBansPlus plugin) {
-        Metrics metrics = new Metrics(plugin, MAXBANS_PLUS_PLUGIN_ID);
+  @Inject
+  public BStatsMetricService(MaxBansPlus plugin) {
 
-        metrics.addCustomChart(new Metrics.SimplePie(LOCALE, () -> plugin.getLocale().getLocale()));
-        metrics.addCustomChart(new Metrics.SingleLineChart(USER_BANS, getAndReset(USER_BANS)));
-        metrics.addCustomChart(new Metrics.SingleLineChart(USER_MUTES, getAndReset(USER_MUTES)));
-        metrics.addCustomChart(new Metrics.SingleLineChart(IP_BANS, getAndReset(IP_BANS)));
-        metrics.addCustomChart(new Metrics.SingleLineChart(IP_MUTES, getAndReset(IP_MUTES)));
-        metrics.addCustomChart(new Metrics.SingleLineChart(WARNINGS, getAndReset(WARNINGS)));
-        metrics.addCustomChart(new Metrics.SingleLineChart(KICKS, getAndReset(KICKS)));
-    }
+  }
 
-    protected Callable<Integer> getAndReset(String key) {
-        return () -> {
-            Integer v = increments.remove(key);
-            if (v == null) return 0;
+  protected Callable<Integer> getAndReset(String key) {
+    return () -> {
+      Integer v = increments.remove(key);
+      if (v == null) return 0;
 
-            return v;
-        };
-    }
+      return v;
+    };
+  }
 
-    public int get(String key) {
-        Integer v = increments.get(key);
-        if(v == null) return 0;
+  public int get(String key) {
+    Integer v = increments.get(key);
+    if (v == null) return 0;
 
-        return v;
-    }
+    return v;
+  }
 
-    @Override
-    public void increment(String chartId) {
-        int value = get(chartId) + 1;
-        increments.put(chartId, value);
-    }
+  @Override
+  public void increment(String chartId) {
+    int value = get(chartId) + 1;
+    increments.put(chartId, value);
+  }
 }
